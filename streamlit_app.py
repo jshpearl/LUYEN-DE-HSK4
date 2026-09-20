b5892a0b6fc6c2387b155778fca84aec94b292b7
Yu <baongoc.elf@gmail.com>
2026-09-20 18:54:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,7 @@ st.markdown("""
 
 CARD_CLASSES = ["card-blue", "card-pink", "card-purple", "card-orange", "card-amber"]
 
-GSHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxfZ7f292zc7Rcq8OdalCQIKl9WDY1fAc21pBMAmXFKr1qnQ3F8FeH-vJqIebuWKQ1U8A/exec"
+GSHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyYtKQHNbMjCi2ZBF3JDUToP5CRvqaheHYDEEwTAQ-dT3lAnSHAozN42Ob1rzFkOwxh/exec"
 
 def send_score_to_gsheet(student_name, exam_code, section_name, score_raw, score_100):
     payload = {
